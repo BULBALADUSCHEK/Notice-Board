31ba654f51e1903ae2fbf80c7f96326b1238c44c
Dasha Shchadinskaya <musiq@yandex.by>
2022-01-26 14:00:40 +0300

    Adding an admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update]
-   
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
   #   def index
   #     @relevant_articles = Article.get_by_state("Relevant")
   #     @expired_articles = Article.get_by_state("Expired")
@@ -32,7 +34,7 @@ class ArticlesController < ApplicationController
 
     def create
       @article = Article.new(article_params)    
-      @article.user = User.first
+      @article.user = current_user
       if @article.save
         flash[:notice] = "Advertisement was created successfully."
         redirect_to @article
@@ -61,6 +63,13 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :description, :expiration_date)
+    end
+
+    def require_same_user
+      if current_user != @article.user && !current_user.admin?
+        flash[:alert] = "Only the owner of a ad can edit or delete a ad"
+        redirect_to @article
+      end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
+  
 
   resources :articles do
     resources :comments     
